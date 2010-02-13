b5a936511d72148c5a6869b4450d9e503075db22
Jathan McCollum <jathan@gmail.com>
2010-02-12 21:05:50 -0800

    added support for 'route' command per issue #1

--- logdrop.py
+++ logdrop.py
@@ -2,7 +2,8 @@
 
 """ 
 Parses thrasher log to watch for activity.  
-Interact with iptables upon detected actions.
+Interact with iptables or route upon detected actions.
+
 """
 
 import commands
@@ -11,66 +12,78 @@ import re
 import sys
 import time
 from optparse import OptionParser
-import platform
 import warnings
-import syslog
 
-__version__ = 0.1
+__version__ = 0.2
 
-log_re  = re.compile(r'^(?P<timestamp>\w{3} \d\d \d\d:\d\d:\d\d) (?P<loghost>\d+\.\d+\.\d+\.\d+) (?P<instance>\S+): (?P<action>holding down|expired) address (?P<attacker>\d+\.\d+\.\d+\.\d+)\s?(?:triggered by (?P<trigger>\d+\.\d+\.\d+\.\d+))?') 
 
+###############################
+##   USER-SERVICABLE PARTS   ##
+###############################
+
+# Default method set here. Change this if you dare!
+DEFAULT_METHOD='iptables'
+
+###############################
+## END USER-SERVICABLE PARTS ##
+###############################
+
+# Interaction method can only be one of the following:
+VALID_METHODS = ('iptables', 'route',)
+
+# Mappings of actions and commands for use in interact()
 ACTIONS = {
-    'holding down': 'I', # I = insert
-    'expired': 'D',      # D = delete
+    'route': {
+        'holding down': 'add',
+        'expired': 'del',
+        'command': "route %s -host %s reject",
+    },
+    'iptables': {
+        'holding down': 'I', # I = insert
+        'expired': 'D',      # D = delete
+        'command': "iptables -%s INPUT -s %s -j DROP",
+    },
 }
 
+# Will be filled up with the activity. Go figure!
 activity = {}
 
-## warnings
-class IPTablesWarning(Warning): 
+# Let's hope this never changes.
+log_re  = re.compile(r'^(?P<timestamp>\w{3} \d\d \d\d:\d\d:\d\d) (?P<loghost>\d+\.\d+\.\d+\.\d+) (?P<instance>\S+): (?P<action>holding down|expired) address (?P<attacker>\d+\.\d+\.\d+\.\d+)\s?(?:triggered by (?P<trigger>\d+\.\d+\.\d+\.\d+))?') 
+
+
+class InteractionWarning(Warning): 
     """
-    If interaction with iptables fails.
+    If interaction with filter method fails.
     """
     pass
 
-## exceptions
-class IPTablesError(Exception): 
+class InteractionError(Exception): 
     """
-    If interaction with iptables causes the program to exit.
+    If interaction with filter method causes the program to exit.
     """
     pass
 
-## functions
-def iptables_interact(parts):
-    """
-    ## add
-    cmd = "iptables -% INPUT -s %s -j DROP" % parts['attacker']
 
-    ## del
-    cmd = "iptables -D INPUT -s %s -j DROP" % parts['attacker']
-    """
-    p = parts
-
-    ## execute
-    cmd = "iptables -%s INPUT -s %s -j DROP" % (ACTIONS[p['action']], p['attacker'])
-    status, output = commands.getstatusoutput(cmd)
-
-    activity[p['attacker']] = (p['action'], status,)
+def interact(parts, method):
+    details = ACTIONS[method]
+    command = details['command'] % (details[parts['action']], parts['attacker'])
+    status, output = commands.getstatusoutput(command)
+    activity[parts['attacker']] = (parts['action'], status,)
     
-    return cmd, status, output
+    return command, status, output
 
 def handle_line(line):
     print line,
 
-    line_parts  = log_re.match(line).groupdict()
-    action      = line_parts['action']
-    attacker    = line_parts['attacker']
+    line_parts = log_re.match(line).groupdict()
+    action = line_parts['action']
+    attacker = line_parts['attacker']
 
-    cmd, status, output = iptables_interact(line_parts)
+    (cmd, status, output) = interact(line_parts, method=opts.method)
 
     if status > 0:
-        #warnings.warn('Problem interacting with iptables for %s' % p['attacker'], IPTablesWarning)
-        warnings.warn('IP %s (%s)' % (attacker, output), IPTablesWarning)
+        warnings.warn('IP %s (%s)' % (attacker, output), InteractionWarning)
         print '\t%s FAILURE' % attacker
     else:
         print '\t%s SUCCESS' % attacker
@@ -145,18 +158,59 @@ def report_activity():
     for ip in activity:
         print ip, activity[ip]
 
-def main(argv=None):
-    parser = OptionParser()
-    parser.add_option("-n", "--number", action="store", type="int", dest = "number", default=10)
-    parser.add_option("-f", "--follow", action="store_true", dest = "follow", default=0)
-    (options, args) = parser.parse_args()
+def parse_args():
+    """
+    What do you think this does?
 
-    do_tail(args[0], options.number, options.follow, handle_line)
+    """
+    USAGE = "usage: %prog [OPTION]... [FILE]..."
+
+    parser = OptionParser(usage=USAGE)
+    parser.add_option("-f", "--follow", action="store_true", dest="follow", default=0,
+                     help="output appended data as the file grows; just like tail")
+    parser.add_option("-n", "--number", action="store", type="int", dest="number", default=10,
+                     help="output  the  last N lines, instead of the last 10; just like tail")
+    parser.add_option("-i", "--iptables", action="store_true", dest="iptables", default=False, 
+                     help="use iptables method [default]; equivalent to --method=iptables")
+    parser.add_option("-r", "--route", action="store_true", dest="route", default=False,
+                     help="use route method; equivalent to --method=route")
+    parser.add_option("-m", "--method", action="store", type="string", dest="method", default=DEFAULT_METHOD,
+                     metavar="METHOD", help="use specified method for interaction")
+
+    (opts, args) = parser.parse_args()
+
+    def _err(msg):
+        parser.print_help()
+        parser.error(msg)
+        sys.exit(-1)
+
+    if not opts or not args: 
+        parser.print_help()
+        sys.exit(0)
+
+    if opts.route and opts.iptables:
+        _err("Can't force -i and -r. Pick one.")
+
+    if opts.method and opts.method not in VALID_METHODS:
+        _err("Pick a valid method: %s" % str(VALID_METHODS))
+
+    if opts.route:
+        opts.method = 'route'
+
+    if opts.iptables:
+        opts.method = 'iptables'
+
+    return opts, args
+
+def main():
+    do_tail(args[0], opts.number, opts.follow, handle_line)
 
 
 if __name__ == "__main__":
+    (opts, args) = parse_args()
+
     try:
-        main(sys.argv)
+        main()
     except KeyboardInterrupt:
         pass
     finally:
